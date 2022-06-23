d05be1fe50d7696ad71e59710e733b409504461e
Alex M <alex@mosstech.io>
2022-06-23 13:50:34 +0100

    Add vulnerability scan stage

---
+++ scripts/scan.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -ouE pipefail
+
+pushd "$(dirname "${BASH_SOURCE[0]}")/../" >/dev/null || exit
+
+# shellcheck disable=SC1091
+. ./scripts/vars.sh
+
+if [[ ! $(which trivy) ]]; then
+    wget https://github.com/aquasecurity/trivy/releases/download/v"${TRIVY_VERSION}"/trivy_"${TRIVY_VERSION}"_Linux-64bit.tar.gz && \
+        tar zxvf trivy_"${TRIVY_VERSION}"_Linux-64bit.tar.gz && \
+        mv trivy /usr/local/bin/trivy
+fi
+
+# not scanning python builder base image - should not be used outside CI
+IMAGES="
+${PYTHON_DISTROLESS_IMAGE}-intermediate-${CI_PIPELINE_ID}
+"
+
+for image in ${IMAGES}; do
+    echo; echo "-> Trivy scan for image: ${image}"; echo
+    trivy image --clear-cache
+    trivy image --exit-code 1 --severity CRITICAL,HIGH --no-progress "${image}"
+done
+
+popd > /dev/null || exit
