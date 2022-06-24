83f0ffeec7390a010be1085d6f8dcd5239ab68cf
Alex M <alex@mosstech.io>
2022-06-24 14:29:29 +0100

    Debug: try skipping tls verify

--- kind_debug.sh
+++ kind_debug.sh
@@ -15,33 +15,35 @@ kind create cluster --name="${CLUSTER}" --config=kind.yaml --wait=60s
 
 # echo "DEBUG: cluster context"
 # cat /root/.kube/config
-cat /etc/hosts
+
 
 # docker ps
 
 kind get kubeconfig --name="${CLUSTER}" | sed -e 's/0.0.0.0/docker/g' > /root/.kube/config
 
-echo "$(dig +short docker) kubernetes" >> /etc/hosts
+# cat /etc/hosts
+# apt-get install -y dnsutils
+# echo "$(dig +short docker) kubernetes" >> /etc/hosts
 
 
-echo "DEBUG: docker"
-curl -v https://docker:6443/api --insecure
+# echo "DEBUG: docker"
+# curl -v https://docker:6443/api --insecure
 
-echo "DEBUG: kubernetes"
-curl -v https://kubernetes:6443/api --insecure
+# echo "DEBUG: kubernetes"
+# curl -v https://kubernetes:6443/api --insecure
 
 
-kubectl config set-cluster kind-${CLUSTER} --server=https://docker:6443
+kubectl config set-cluster kind-${CLUSTER} --server=https://docker:6443 --insecure-skip-tls-verify=true
 
 echo "DEBUG: get cluster info (docker)"
 kubectl cluster-info --context kind-${CLUSTER}
 kubectl get ns --context kind-${CLUSTER}
 
-kubectl config set-cluster kind-${CLUSTER} --server=https://kubernetes:6443
+# kubectl config set-cluster kind-${CLUSTER} --server=https://kubernetes:6443 --insecure-skip-tls-verify=true
 
-echo "DEBUG: get cluster info (kubernetes)"
-kubectl cluster-info --context kind-${CLUSTER}
-kubectl get ns --context kind-${CLUSTER}
+# echo "DEBUG: get cluster info (kubernetes)"
+# kubectl cluster-info --context kind-${CLUSTER}
+# kubectl get ns --context kind-${CLUSTER}
 
 
 # # believe it or not this seems easier than getting .kube/config to work inside distroless ...
